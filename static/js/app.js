0636527b8f4e88723d6bd5fc41af2883ca95e28f
Valeriy Shvets <steelval@gmail.com>
2014-11-22 23:12:56 +0200

    pagination added

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,44 @@
 
 (function (){
+
+    function getURLParameter(name) {
+        var rex = RegExp(name + '=' + '(.+?)(&|$)');
+        var result = rex.exec(location.search);
+        var rv = decodeURI(
+            (result||[,null])[1]
+        );
+        return rv;
+    }
+
+    function makeUrl() {
+        var url = 'http://www.reddit.com/r/all/.json?count=25&'
+        var after = getURLParameter("after");
+        var before = getURLParameter("before");
+        url += (typeof after != "undefined" ? "&after=" + after : "");
+        url += (typeof before != "undefined" ? "&before=" + before : "");
+        return url
+    }
+
     var app = angular.module('reddit', []);
 
-    app.controller('RedditController', ['$http', function($http){
+    app.controller('RedditController', ['$http', '$scope', function($http, $scope){
         var all_pics = this;
-        all_pics.pics = [];
 
-        $http.get('http://www.reddit.com/r/pics/.json?&jsonp=').success(function(data){
-            asd = data.data.children
+        // initial state
+        all_pics.pics = [];
+        var url = makeUrl();
+        var after = null;
+        var before = null;
+
+        $scope.isAfter = function(){return !!after;}
+        $scope.isBefore = function(){return !!before;}
+
+        $http.get(url).success(function(data){
+            before = data.data.children[0].data.name;
+            after = data.data.children.pop().data.name;
+
+            $scope.urlBefore = "?before=" + before;
+            $scope.urlAfter = "?after=" + after;
             all_pics.pics = data.data.children;
         });
     }]);
@@ -48,10 +79,10 @@
             var difference = now - created_utc*1000;
 
             var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
-            difference -= daysDifference * 1000 * 60 * 60 * 24
+            difference -= daysDifference * 1000 * 60 * 60 * 24;
 
             var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
-            difference -= hoursDifference * 1000 * 60 * 60
+            difference -= hoursDifference * 1000 * 60 * 60;
 
             var minutesDifference = Math.floor(difference / 1000 / 60);
 
